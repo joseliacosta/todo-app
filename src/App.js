b14bc87b6733177aaa8b8810714e31a955b77814
Josélia L. Costa <joselialcosta@gmail.com>
2020-10-10 15:38:45 -0300

    feat(app): set initial config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>To do list</h1>
       </header>
+      <main>
+        <ul>
+          <li>
+            <input type="checkbox" />
+            Study
+          </li>
+          <li>Cook dinner</li>
+          <li>Play Luigi's mansion</li>
+        </ul>
+        <input placeholder="Write a task" />
+        <select>
+          <option>Category</option>
+          <option value="work">work</option>
+          <option value="home">home</option>
+        </select>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
