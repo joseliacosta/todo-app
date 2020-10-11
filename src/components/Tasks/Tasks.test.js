180751f16c95d0b854e4ab5f99e685cc76d6b3e3
Josélia L. Costa <joselialcosta@gmail.com>
2020-10-11 18:39:09 -0300

    refactor(tasks): improves task and tasks styles and add category prop to task component

--- src/components/Tasks/Tasks.test.js
+++ src/components/Tasks/Tasks.test.js
@@ -1,9 +1,53 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
-import Tasks from '.'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { Tasks, Task } from '.'
 
 test('render 5 items when component receive a list with size=5 from parent', () => {
   const mockList = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5']
   render(<Tasks list={mockList} />)
   expect(screen.queryAllByRole('listitem').length).toEqual(5)
+})
+
+test('Marks task as checked when checkbox is clicked', () => {
+  const toDoMock = {
+    text: 'Do laundry',
+    category: 'home',
+    isCompleted: false,
+  }
+  const completeTodoSpy = jest.fn()
+
+  render(<Task todo={toDoMock} index={0} completeTodo={completeTodoSpy} />)
+
+  fireEvent.click(screen.getByRole('checkbox'))
+
+  expect(completeTodoSpy).toHaveBeenCalled()
+})
+
+test('Renders category label if task have one', () => {
+  const completeTodoSpy = jest.fn()
+  const toDoComplete = {
+    text: 'Do laundry',
+    category: 'home',
+    isCompleted: false,
+  }
+
+  render(<Task todo={toDoComplete} index={0} completeTodo={completeTodoSpy} />)
+
+  expect(screen.getByText('home')).toBeVisible()
+})
+
+test('Do not renders category label when task does not have one', () => {
+  const completeTodoSpy = jest.fn()
+
+  const toDoWithoutCategory = {
+    text: 'Meditation',
+    category: '',
+    isCompleted: false,
+  }
+
+  render(
+    <Task todo={toDoWithoutCategory} index={0} completeTodo={completeTodoSpy} />
+  )
+
+  expect(screen.queryByRole('label')).toBeNull()
 })
--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -3,30 +3,64 @@ import styled from 'styled-components'
 
 const StyledTasks = styled.ul`
   list-style: none;
+  max-width: 320px;
 `
-const Items = styled.li`
-  display: flex;
-  align-items: center;
-  min-height: 50px;
-  border-bottom: 1px solid #ebebeb;
-  input[type='checkbox'] {
-    margin-right: 12px;
-  }
-`
+
 const Tasks = (props) => {
-  const { list } = props
+  const { list, completeTodo } = props
+
   return (
     <StyledTasks>
-      {list.map((item) => {
+      {list.map((todo, index) => {
         return (
-          <Items key={`${item}--task`}>
-            <input type="checkbox" />
-            {item}
-          </Items>
+          <Task
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
         )
       })}
     </StyledTasks>
   )
 }
 
-export default Tasks
+const StyledTask = styled.li`
+  display: flex;
+  align-items: center;
+  background: #fff;
+  border-radius: 3px;
+  border: 1px solid #ebebeb;
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.15);
+  font-size: 16px;
+  height: 32px;
+  margin-bottom: 6px;
+  padding: 12px;
+
+  input[type='checkbox'] {
+    margin-right: 12px;
+  }
+`
+const Description = styled.span`
+  text-decoration: ${(props) => props.isCompleted && 'line-through'};
+`
+const Category = styled.label`
+  font-weight: 700;
+  background: #ebebeb;
+  border-radius: 4px;
+  padding: 5px;
+  margin-left: auto;
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.15);
+`
+
+const Task = ({ todo, index, completeTodo }) => {
+  return (
+    <StyledTask>
+      <input type="checkbox" onClick={() => completeTodo(index)} />
+      <Description isCompleted={todo.isCompleted}>{todo.text}</Description>
+      {todo.category && <Category>{todo.category}</Category>}
+    </StyledTask>
+  )
+}
+
+export { Task, Tasks }
