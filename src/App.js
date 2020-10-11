161d7a40ee39663bc6022cb3f35b1e6df47aa58f
Josélia L. Costa <joselialcosta@gmail.com>
2020-10-11 18:44:27 -0300

    refactor(app): controls initial tasks by state and add function to mark tasks as complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import Filter from './components/Filter'
-import Form from './components/Form'
-import Tasks from './components/Tasks'
+import { Tasks } from './components/Tasks'
 
 function App() {
-  const list = [
-    'Laundry',
-    'Cook dinner',
-    'Study hooks',
-    'send PR to Hacktober Fest',
-  ]
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'Learn about React',
+      category: 'work',
+      isCompleted: false,
+    },
+    {
+      text: 'Do laundry',
+      category: 'home',
+      isCompleted: false,
+    },
+    {
+      text: 'Build really cool todo app',
+      category: 'work',
+      isCompleted: false,
+    },
+    {
+      text: 'Meditation',
+      category: '',
+      isCompleted: false,
+    },
+  ])
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = true
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <h1>To do list</h1>
         <Filter />
       </header>
       <main>
-        <Tasks list={list} />
-        <Form />
+        <Tasks list={todos} completeTodo={completeTodo}></Tasks>
       </main>
     </div>
   )
